cf6344892c4ed6c2a85fcb00b4f277815fb27cc2
顾融章 <gurongzhang@gurongzhangdeMacBook-Pro.local>
2019-04-11 19:13:25 +0800

    add comments for s4

--- Session4/S4-MyExamples-00-ArrayMeshPractice/js/index.js
+++ Session4/S4-MyExamples-00-ArrayMeshPractice/js/index.js
@@ -1,37 +1,46 @@
 var renderer, scene, camera, mesh;
 var controls;
-var cubes = [];//array system
+// Array
+var cubes = [];
 var rot = 0;
 
+// Basic settings(scene, camera, spotLight, renderer)
+// OrbitControls
+// Mesh settings
 function init() {
+  // Add scene
   scene = new THREE.Scene();
 
-  var W = window.innerWidth,
-  H = window.innerHeight;
-
+  var W = window.innerWidth,H = window.innerHeight;
+  // Camera settings
   camera = new THREE.PerspectiveCamera(20, W / H, .1, 1000);
   camera.position.set(10, 10, 85);
   camera.lookAt(scene.position);
-
+  // Set spotLight
   var spotLight = new THREE.SpotLight(0xFFFFFF);
   spotLight.position.set(0, 1000, 0);
   scene.add(spotLight);
-  //spotLight.castShadow = true;
-
+  // Set renderer
   renderer = new THREE.WebGLRenderer({antialias:true});
   renderer.setClearColor(0x17293a);
   renderer.setSize(W, H);
 
-  //renderer.shadowMapEnabled = true;
-controls = new THREE.OrbitControls(camera, renderer.domElement);
-  //Create a two dimensional grid of objects, and position them accordingly
-  for (var x = -10; x < 10; x += 5) { // Start from -45 and sequentially add one every 5 pixels
+  // Append Renderer to DOM
+  document.body.appendChild(renderer.domElement);
+
+  // Set OrbitControls
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  //Create a three dimensional grid of objects, and position them accordingly
+  // Start from -10 and sequentially add one every 5 pixels
+  for (var x = -10; x < 10; x += 5) {
     for (var y = -10; y < 10; y += 5) {
       for (var z = -10; z < 10; z += 5) {
-        console.log("X:"+x,"Y:"+y,"Z:"+z)
+        // Create a Cube Mesh with Lambert material
         var boxGeometry = new THREE.BoxGeometry(3, 6, 3);
-        //The color of the material is assigned a random color
-        //color: Math.random() * 0xFFFFFF
+        // Set mesh
+        mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+        // Define the conditions for mesh colors
         if(x >= 0 && y>= 0 && z >= 0){
           var boxMaterial = new THREE.MeshLambertMaterial({color:0xFF00FF});
         }
@@ -56,31 +65,34 @@ controls = new THREE.OrbitControls(camera, renderer.domElement);
         if(x < 0 && y< 0&&z < 0){
           var boxMaterial = new THREE.MeshLambertMaterial({color: 0xDC143C});
         }
-        mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-        //mesh.castShadow = true;
+        // Set mesh postions
         mesh.position.x = x;
         mesh.position.y = y;
         mesh.position.z = z;
+        // Set mesh size
         mesh.scale.y = 0.5;
+        // Add mesh to scene
         scene.add(mesh);
+        // Push all cubes into array
         cubes.push(mesh);
       }
     }
   }
-  document.body.appendChild(renderer.domElement);
 }
 
+
+// Set animations
 function drawFrame(){
   requestAnimationFrame(drawFrame);
+ // Set rotation speed
  rot += 0.02;
  cubes.forEach(function(c, i){
-   c.rotation.x = rot; //Math.random() * 2 * Math.PI;
-    c.rotation.y = rot;
-     c.rotation.z = rot;
+   c.rotation.x = rot;
+   c.rotation.y = rot;
+   c.rotation.z = rot;
  });
   renderer.render(scene, camera);
 }
-
 
 
 init();
